e5522b74e789b305319c6cb6716df31c5c4f021b
Ishara <ishara.macdonald@student.hu.nl>
2020-03-10 20:23:17 +0100

    Finished my part
    Klassen Afmelding, ZiekMelding en Student afgerond. Enum PresentieStatus, CollegeType en TijdBlok toegevoegd. Enkele Attributen toegevoegd bij klasse College om de rest werkend te krijgen.

--- Coding/Testing/CodeOnly/src/College.java
+++ Coding/Testing/CodeOnly/src/College.java
@@ -1,2 +1,15 @@
 public class College {
+    private String naam;
+
+    private TijdBlok tijdBlok;
+    private CollegeType collegeType;
+
+
+    public void setTijd(TijdBlok tijdBlok){this.tijdBlok = tijdBlok;}
+    public void setType(CollegeType collegeType){this.collegeType = collegeType;}
+    public void setNaam(String naam){this.naam = naam;}
+
+    public String getTijd(){return tijdBlok.toString();}
+    public CollegeType getType(){return collegeType;}
+    public String getNaam(){return naam;}
 }
