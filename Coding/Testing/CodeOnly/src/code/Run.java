18f22faa437fbc84862354f32c5baa2ef14a14fa
aegroot <alex.a.groot@student.hu.nl>
2020-03-25 13:15:39 +0100

    update rrd

--- Coding/Testing/CodeOnly/src/code/Run.java
+++ Coding/Testing/CodeOnly/src/code/Run.java
@@ -1,15 +1,34 @@
 package code;
 
+import util.ReadDoc;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.TreeMap;
 
 public class Run {
     public static void main(String[] args) {
-        LocalDateTime nu=LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        String na=nu.format(formatter);
-        System.out.println(nu);
-        System.out.println(na);
+
+//        LocalDateTime nu=LocalDateTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+//        String na=nu.format(formatter);
+//        System.out.println(nu);
+//        System.out.println(na);
+        TreeMap<Gebruiker,String> accounting= ReadDoc.readShadow();
+        Collection<String> keys = accounting.values();
+        for (String str:keys
+             ) {System.out.println(str);
+
+        }
+
+
+
+
+
+        }
+
+
 
     }
-}
+
