558ef71bd0dba62489bfa714b1951bf0900d20d8
aegroot <alex.a.groot@student.hu.nl>
2020-03-25 14:21:43 +0100

    update rrd

--- Coding/Testing/CodeOnly/src/util/ReadDoc.java
+++ Coding/Testing/CodeOnly/src/util/ReadDoc.java
@@ -9,18 +9,23 @@ import java.nio.file.Path;
 import java.util.TreeMap;
 
 public class ReadDoc {
-    public static TreeMap<Gebruiker,String> readShadow() {
-        String doc="CodeOnly/src/shadow";
+    public static TreeMap<String,String> readShadow() {
+        String doc="shadow";
         Path pdoc=Path.of(doc);
-        TreeMap<Gebruiker,String> accounts= new TreeMap<>();
+        TreeMap<String,String> accounts= new TreeMap<>();
         System.out.println(pdoc);
         try {
             BufferedReader reader= Files.newBufferedReader(pdoc);
             String text=reader.readLine();
             while(text!=null){
             String[] lijst=text.split(" : ");
-                    accounts.put(new Gebruiker(lijst[0],lijst[1]),lijst[2]);
-                    text=reader.readLine();
+
+                String username=lijst[0];
+                String password=lijst[1];
+                String acces=lijst[2];
+                Gebruiker gebr=new Gebruiker(username,password);
+                accounts.put(lijst[0],lijst[1]);
+                text=reader.readLine();
 
 
             }
