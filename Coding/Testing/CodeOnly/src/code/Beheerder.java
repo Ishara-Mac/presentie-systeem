c5808eb6dc4f9ce9bac71183854c62e40de04319
aegroot <alex.a.groot@student.hu.nl>
2020-03-25 23:35:01 +0100

    update rrd

--- Coding/Testing/CodeOnly/src/code/Beheerder.java
+++ Coding/Testing/CodeOnly/src/code/Beheerder.java
@@ -1,7 +1,5 @@
 package code;
 
-public class Beheerder extends Gebruiker{
-    public Beheerder(String username, String wachtwoord) {
-        super(username, wachtwoord);
-    }
+public class Beheerder{
+
 }
