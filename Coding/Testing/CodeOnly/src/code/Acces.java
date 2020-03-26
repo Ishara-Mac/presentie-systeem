f2c82a72a0346a9f8c4b69e5c7b7ead531d9ef3b
aegroot <alex.a.groot@student.hu.nl>
2020-03-26 13:26:39 +0100

    update rrd

--- Coding/Testing/CodeOnly/src/code/Acces.java
+++ Coding/Testing/CodeOnly/src/code/Acces.java
@@ -4,6 +4,7 @@ public class Acces {
     private String username;
     private String wachwoord;
     private String acces;
+    private boolean loggedin=false;
 
     public Acces(String username, String wachwoord,String acces) {
         this.username=username;
