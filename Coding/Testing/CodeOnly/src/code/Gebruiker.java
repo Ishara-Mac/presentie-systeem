f937bd16b9dfc2587787bd051d6670c45abd2eee
Ishara-MacDonald <ishara.macdonald@student.hu.nl>
2020-03-28 14:53:37 +0100

    Added Docent and Student Users

--- Coding/Testing/CodeOnly/src/code/Gebruiker.java
+++ Coding/Testing/CodeOnly/src/code/Gebruiker.java
@@ -1,7 +1,22 @@
 package code;
 
-public class Gebruiker  extends Acces{
+import java.util.ArrayList;
+
+public class Gebruiker{
+    private String naam;
+    private String wachwoord;
+    private String acces;
     public Gebruiker(String username,String password,String acces) {
-        super(username,password,acces);
+    }
+
+    public Gebruiker(String naam){
+        this.naam = naam;
+    }
+
+    public String getNaam(){return naam;}
+
+    public ArrayList<RoosterRegel> procesRooster(){
+        System.out.println("Methode niet gemaakt in subklasse!");
+        return null;
     }
 }
